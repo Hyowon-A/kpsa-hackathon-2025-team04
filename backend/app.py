c90982c699225020cca78873705cb497d7c2d26e
Hyowon Ahn <hyowon.ahn@student.mancehster.ac.uk>
2025-07-27 04:11:28 +0900

    render

--- backend/app.py
+++ backend/app.py
@@ -25,4 +25,5 @@ def index():
     return "Backend server is running!"
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
